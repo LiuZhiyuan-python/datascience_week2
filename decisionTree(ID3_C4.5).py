466128f21804e9064120d81490f99a56fa841440
LiuZhiyuan-python <3520228847@qq.com>
2021-04-29 13:52:13 +0800

    解决了先前的bug，跟原作者讨论了一下，确认了这个错误

--- decisionTree(ID3_C4.5).py
+++ decisionTree(ID3_C4.5).py
@@ -46,12 +46,28 @@ class DecisionTree(object):
         # 遍历特征所有的取值，
         fea_idx = tree.feature_idx
         for fea_val, child_node in tree.child.items():
+            # if fea_val == 3.0 or 3.2:
+            #     # print(x[fea_idx], fea_val, child_node, child_node.node_val)
             if x[fea_idx] == fea_val:
                 if child_node.node_val is not None:
                     return child_node.node_val
                 else:
-                    if tree != self.root:
-                        print(fea_val)
+                    """
+                    错误原因在此处：
+                    每次出错的数据都是[5.9 3.2 1.8 4.8] 和 [5.9 3.0 5.1 1.8]。
+                    决策树中这两个数据对应的节点能够查到node_val值，但是预测不出来，说明是predict_help函数出了问题。
+                    检查之后发现，predict_help函数中，每次递归用到的x都时一样的，但是在建立树时，每次会删掉一列。
+                    因此，两者用到的索引意义是不同的，这里存在一个错误。
+                    具体到这两个数据集，函数执行到fea_idx = tree.feature_idx时:
+                    开始这个索引是3，x[3]==1.8。
+                    接下来是0，x[0]==5.9,
+                    但接下来，索引依然是0（创造子树时把x显著特征的列删掉了）,对应x[0]==5.9不是3.0或者3.2，因此这里没有返回值。
+                    """
+                    try:
+                        x = np.delete(x, fea_idx, 1)
+                    except:
+                        x = np.delete(x, fea_idx, 0)
+
                     return self.predict_help(x, child_node)
 
     def build_tree(self, X, y):
@@ -62,42 +78,44 @@ class DecisionTree(object):
             return TreeNode(node_val=y[0])
 
         # 2. 不能继续划分，置T为单节点树，将最多的类别标签该节点的类别标签
-        if X.shape[1] == 1:
+        elif X.shape[1] == 1:
             node_val = self.vote_label(y)
             return TreeNode(node_val=node_val)
 
         # 3. 否则，计算各特征的信息增益，记录最大的信息增益和对应的特征
-        n_feature = X.shape[1]
-        # 将最大增益（比）初始化为负无穷
-        max_gain = -np.inf
-        max_fea_idx = 0
-        for i in range(n_feature):
-            # 传入第i维特征的值，计算信息增益（比）
-            if self.etype == "gain":
-                gain = self.calc_gain(X[:, i], y)
-            else:
-                gain = self.calc_gain_ratio(X[:, i], y)
-            # 记录最大增益值和对应的维度i
-            if gain > max_gain:
-                max_gain = gain
-                max_fea_idx = i
+        else:
+                n_feature = X.shape[1]
+                # 将最大增益（比）初始化为负无穷
+                max_gain = -np.inf
+                max_fea_idx = 0
+                for i in range(n_feature):
+                    # 传入第i维特征的值，计算信息增益（比）
+                    if self.etype == "gain":
+                        gain = self.calc_gain(X[:, i], y)
+                    else:
+                        gain = self.calc_gain_ratio(X[:, i], y)
+                    # 记录最大增益值和对应的维度i
+                    if gain > max_gain:
+                        max_gain = gain
+                        max_fea_idx = i
 
-        # 4.信息增益(比)小于epsilon，置T为单节点树
-        if max_gain <= self.epsilon:
-            node_val = self.vote_label(y)
-            return TreeNode(node_val=node_val)
+                # 4.信息增益(比)小于epsilon，置T为单节点树
+                if max_gain <= self.epsilon:
+                    node_val = self.vote_label(y)
+                    return TreeNode(node_val=node_val)
 
-        # 5.否则，对特征的每一个取值构建子结点，返回结点和子结点形成的树T
-        feature_name = self.feature_name[max_fea_idx]
-        child_tree = dict()
-        # 构建一维子树，删除一个特征
-        # 作递归，直到所有的树枝都达到叶结点
-        for fea_val in np.unique(X[:, max_fea_idx]):
-            child_X = X[X[:, max_fea_idx] == fea_val]
-            child_y = y[X[:, max_fea_idx] == fea_val]
-            child_X = np.delete(child_X, max_fea_idx, 1)
-            child_tree[fea_val] = self.build_tree(child_X, child_y)
-        return TreeNode(max_fea_idx, feature_name=feature_name, child=child_tree)
+                # 5.否则，对特征的每一个取值构建子结点，返回结点和子结点形成的树T
+                else:
+                    feature_name = self.feature_name[max_fea_idx]
+                    child_tree = dict()
+                    # 构建一维子树，删除一个特征
+                    # 作递归，直到所有的树枝都达到叶结点
+                    for fea_val in np.unique(X[:, max_fea_idx]):
+                        child_X = X[X[:, max_fea_idx] == fea_val]
+                        child_y = y[X[:, max_fea_idx] == fea_val]
+                        child_X = np.delete(child_X, max_fea_idx, 1)
+                        child_tree[fea_val] = self.build_tree(child_X, child_y)
+                    return TreeNode(max_fea_idx, feature_name=feature_name, child=child_tree)
 
     def vote_label(self, y):
         """统计剩余的样本中次数出现最多的类别作为叶节点的类别"""
@@ -154,7 +172,7 @@ def main():
         else:
             print(xtrain[i], ytrain[i])
             logger.info(f"这个样本点所得到的预测类别是{y_pred}，但真实类别是{ytrain[i]}")
-    logger.info(f"模型预测的准确率是{n_right*100/n_test}%")
+    logger.info(f"模型预测的准确率是{n_right * 100 / n_test}%")
     print('complete')
     """
     bug描述：有一定几率会出现概率不为100%的情况
